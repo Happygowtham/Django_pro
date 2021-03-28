e9441362ec07bb81c12161e392de3861c701ca1a
unknown <gowtham06787@gmail.com>
2021-03-28 15:50:38 +0530

    First Commit

---
+++ sampleapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+Urlpatterns = [
+	path("index", views.index, name="index"),
+    path("login",views.login, name="login"),
+    path("home",views.home, name="home"),
+    path("logout",views.logout, name= "logout"),
+]
